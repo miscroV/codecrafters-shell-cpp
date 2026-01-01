8b791c83f2443373916fbab97afc1fa058747878
Miscro V <miscrov@outlook.com>
2026-01-01 16:57:17 -0600

    Basic REPL loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,17 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  while (true) {
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+    // TODO: Uncomment the code below to pass the first stage
+    std::cout << "$ ";
 
-  std::string command;
-  std::getline(std::cin, command);
+    std::string command;
+    std::getline(std::cin, command);
 
-  std::cout << command << ": command not found" << std::endl;
+    std::cout << command << ": command not found" << std::endl;
+  }
 }
