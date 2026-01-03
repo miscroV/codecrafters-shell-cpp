01378f95427e3cb62d090be6e507f88405916ee7
Miscro V <miscrov@outlook.com>
2026-01-03 09:43:39 -0600

    testing commmand

--- src/main.cpp
+++ src/main.cpp
@@ -82,8 +82,9 @@ int handle_commands(std::string command, std::vector<std::string> args) {
   }
   // Execute external command
   else if (fs::path execpath = get_executable_path(command); !execpath.empty()) {
-      bp::child c(
-          execpath.filename().generic_string(),
+    args.insert(args.begin(), command);  
+    bp::child c(
+          // execpath.generic_string(),
           bp::args(args),
           bp::std_out > stdout,
           bp::std_err > stderr
