d79229616bc44cb7a9d81fd665269307e624a26c
miscroV <miscroV@outlook.com>
2026-01-15 21:08:09 +0000

    split handlers files

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@
 #include <sstream>
 #include <algorithm>
 #include <filesystem>
-#include <boost/process.hpp>
 #include <iomanip>
 #include <stdexcept>
 
@@ -19,20 +18,6 @@
 
 namespace fs = std::filesystem;
 namespace bp = boost::process;
-
-// COMMAND HANDLER FUNCTION DECLARATION ----------------------------------------
-
-/**
- * @brief Command Switch / Handler Function
- * 
- * @param command String value of command to be ran.
- * @param args Vector array of arguments to send to the command
- * @return Int error code returned by the command. 
- * 
- * @note Native commands are {exit, echo, type}
- * 
- */
-int handle_commands(std::string command, std::vector<std::string> args);
 
 // MAIN PROGRAM LOOP -----------------------------------------------------------
 
@@ -90,38 +75,4 @@ int main() {
   }
 }
 
-// COMMAND HANDLER FUNCTION ----------------------------------------------------
 
-int handle_commands(std::string command, std::vector<std::string> args) {
-  // list of default commands that handle_commands handles natively. 
-  std::vector<std::string> native_commands = {
-    "exit", "echo", "type", // Basic Commands
-    "pwd", "cd"             // Navigation Commands
-  };
-  std::sort(native_commands.begin(), native_commands.end());
-
-  // native commands switch
-  if      (command == "exit") { return exit(); }
-  else if (command == "echo") { return echo(args); }
-  else if (command == "type") { return type(args, native_commands); }
-  else if (command == "cd"  ) { return cd(args); }
-  else if (command == "pwd" ) { return pwd(); }
-  // default to finding command in path and run child process
-  else if (fs::path execpath = get_executable_path(command); 
-    !execpath.empty()) {
-    args.insert(args.begin(), command);  
-    bp::child c(
-      bp::args(args),
-      bp::std_out > stdout,
-      bp::std_err > stderr,
-      bp::std_in  < stdin
-      );
-    c.wait();
-    return c.exit_code();
-  }
-  // if command not found:
-  else {
-    std::cout << command << ": command not found" << std::endl;
-  return 0;
-  }
-}   
